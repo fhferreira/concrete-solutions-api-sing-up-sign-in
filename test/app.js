b29a6f552b0d36742bd4151db3dd8a315fb7259c
astesio <astesiojose@gmail.com>
2016-10-28 23:54:16 -0200

    change test

--- test/app.js
+++ test/app.js
@@ -3,11 +3,11 @@ import { request, expect } from './helpers';
 
 describe('Routes Index', () => {
     describe('Route GET /', () => {
-        it('should return a 200 response', (done) => {
+        it('should return a 404 response', (done) => {
             request
                 .get('/')
                 .set('Accept', 'application/json')
-                .expect(200, done);
+                .expect(404, done);
         });
     });
 });
