40b268ef26eae65a61941c0e44fd33ad55f725c4
astesio <astesiojose@gmail.com>
2016-10-29 00:46:03 -0200

    validate update_at date after created_at

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,16 +1,16 @@
 import mongoose from 'mongoose';
-let Schema = mongoose.Schema;
 
-let userSchema = new Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    telephones: [{ ddd: String, number: String }],
-    created_at: { type: Date, default: Date.now() },
-    updated_at: Date,
-    last_login: Date,
-    token: String
-});
+let Schema  = mongoose.Schema,
+    userSchema = new Schema({
+        name: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        telephones: [{ ddd: String, number: String }],
+        created_at: { type: Date, default: Date.now() },
+        updated_at: Date,
+        last_login: Date,
+        token: String
+    });
 
 userSchema.methods.hasPassword = () => {
     console.log('hasPassword');
@@ -19,11 +19,12 @@ userSchema.methods.hasPassword = () => {
 // I have a problem here with my arrow function uses lexical for updated the filed updated_at
 userSchema.pre('save', function(next) {
     const currentDate = new Date();
-    this.updated_at = currentDate;
 
     if(!this.created_at) {
         this.created_at = currentDate;
     }
+
+    this.updated_at = currentDate;
 
     next();
 });
