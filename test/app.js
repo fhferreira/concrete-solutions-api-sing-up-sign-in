4fa1a8cf5528441e2c560822ac4a76663c82f495
astesio <astesiojose@gmail.com>
2016-10-29 00:03:24 -0200

    add a new test 404 status code for an endpoint /api

--- test/app.js
+++ test/app.js
@@ -10,4 +10,13 @@ describe('Routes Index', () => {
                 .expect(404, done);
         });
     });
+
+    describe('Route GET /api', () => {
+        it('should return a 404 response not found', (done) => {
+            request
+                .get('/api')
+                .set('Accept', 'application/json')
+                .expect(404, done);
+        });
+    });
 });
